8a14556f0d6dff7307857c8213868c7bca90c6db
idAndrEk <AndrEkat96@yandex.ru>
2025-06-16 12:25:08 +0500

    Обновил в monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $(ps aux | grep nginx | grep -v grep | wc -l) -eq 0 ]; then
+if ! pgrep redis-server > /dev/null; then
     echo "Nginx не запущен: $(date)" >> ~/portfolio/project_Lin/monitor.log
 else
     echo "Nginx работает: $(date)" >> ~/portfolio/project_Lin/monitor.log
