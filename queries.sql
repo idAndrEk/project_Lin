c1be7ea714695911cb77e9a0d7a3a0f22495bcc2
idAndrEk <AndrEkat96@yandex.ru>
2025-06-16 11:45:51 +0500

    Обновил SQL-запросы в queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,7 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(50));
 INSERT INTO users (name) VALUES ('Andrey'), ('Bob');
 SELECT * FROM users;
+
+#
+UPDATE users SET name = 'TEST' WHERE id = 1;
+DELETE FROM users WHERE name = 'TEST';
