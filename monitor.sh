17f449fca3063ac3e5009a50af5bc3c4faf9aabb
idAndrEk <AndrEkat96@yandex.ru>
2025-06-16 12:13:43 +0500

    Обновил в monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $(ps aux | grep nginx | grep -v grep | wc -l) -eq 0 ]; then
-    echo "Nginx не запущен: $(date)" >> ~/monitor.log
+    echo "Nginx не запущен: $(date)" >> ~/portfolio/project_Lin/monitor.log
 else
-    echo "Nginx работает: $(date)" >> ~/monitor.log
+    echo "Nginx работает: $(date)" >> ~/portfolio/project_Lin/monitor.log
 fi
